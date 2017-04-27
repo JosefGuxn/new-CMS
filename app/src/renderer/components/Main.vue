dcaf8f43745e32abe01d0f2d6c5ba019935872b0
JosefGuxn <josefguxn@gmail.com>
2017-04-27 22:13:12 +0700

    Header nav bar

--- app/src/renderer/components/Main.vue
+++ app/src/renderer/components/Main.vue
@@ -2,15 +2,15 @@
   <div>
     <header class="header">
       <el-row>
-        <el-col :span="6">
+        <el-col :span="4">
           <h1>Clinic</h1>
         </el-col>
-        <el-col :span="18">
-          <el-menu mode="horizontal" router>
-            <el-menu-item index="/main/consultation">Khám bệnh</el-menu-item>
-            <el-menu-item index="/">Thẩm mỹ</el-menu-item>
-            <el-menu-item index="/">Thanh toán</el-menu-item>
-            <el-menu-item index="/">Quản lý</el-menu-item>
+        <el-col :span="20">
+          <el-menu theme="dark" class="nav-menu" mode="horizontal" router>
+            <el-menu-item class="nav-item" index="/"><span>Quản Lý</span></el-menu-item>
+            <el-menu-item class="nav-item" index="/"><span>Thanh Toán</span></el-menu-item>
+            <el-menu-item class="nav-item" index="/"><span>Thẩm Mỹ</span></el-menu-item>
+            <el-menu-item class="nav-item" index="/main/consultation"><span>Khám Bệnh</span></el-menu-item>
           </el-menu>
         </el-col>
       </el-row>
@@ -33,9 +33,19 @@
     height: 80px;
   }
   .el-row {
-    height: 100%;
+    height: 80px;
   }
-
+  .nav-menu {
+    height: 80px;
+  }
+  .nav-item {
+    height: 80px;
+    font-size: 20px;
+    float: right;
+  }
+  .nav-item span {
+    vertical-align:-10px;
+  }
   .main-cnt {
     min-height: 100%;
   }
