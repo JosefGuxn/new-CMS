7622ce8299cd05a34c75846311d18984dc91a35a
JosefGuxn <josefguxn@gmail.com>
2017-05-04 10:59:11 +0700

    improve IU

--- app/src/renderer/components/Clinic.vue
+++ app/src/renderer/components/Clinic.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <h2>Clinic</h2>
-    <router-link to="/main/clinic/consultation">Khám</router-link>
-    <router-view></router-view>
+  <div class="clinic-cnt">     
+    <el-menu class="sidebar" theme="dark" router>
+      <el-menu-item index="/main/clinic/consultation"><i class="el-icon-menu"></i></el-menu-item>
+      <el-menu-item index="/"><i class="el-icon-setting"></i></el-menu-item>
+    </el-menu>
+    <router-view class="router-view"></router-view> 
   </div>
 </template>
+
+<script>
+</script>
+
+<style>
+  .clinic-cnt {
+    height: 100%;
+    width: 100%;
+    display: flex;
+  }
+  .sidebar {
+    width: 60px;
+  }
+  .router-view {  
+    flex: 1 100%;
+  }
+</style>
